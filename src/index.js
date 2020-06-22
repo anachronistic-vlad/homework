b84505b44ca7a061bc59e8f306aa6d869b5dc38a
Vladyslav <vegabund66@gmail.com>
2020-06-23 02:24:07 +0300

    HomeWork finished

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 let age = +prompt('Каков ваш возраст?');
-let ageOne = (age % 10);
 
-if (ageOne === 0) {
+if (age >= 5 && age < 20) {
     alert('Вам ' + age + ' лет');
-} else if (ageOne === 1) {
+} else if ((age % 10) === 0) {
+    alert('Вам ' + age + ' лет');
+} else if ((age % 10) === 1) {
     alert('Вам ' + age + ' год');
-} else if (ageOne < 5) {
+} else if ((age % 10) < 5) {
     alert('Вам ' + age + ' года');
-} else if (ageOne >= 5) {
+} else if ((age % 10) >= 5) {
     alert('Вам ' + age + ' лет');
 }
