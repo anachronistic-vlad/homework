2eb4e204cdff8849ab361a0fe6063a09b64f06bc
Vladyslav <vegabund66@gmail.com>
2020-06-24 08:42:37 +0300

    Убрал кусочек кода, который оказался лишним

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 "use strict";
 let age = +prompt('Каков ваш возраст?');
 
-if ((age >= 5) && (age <= 20) || (age % 10 === 0) || (age % 10) >= 5 || (age % 100 >= 11) && (age % 100 <= 15)) {
+if ((age % 10 === 0) || (age % 10) >= 5 || (age % 100 >= 11) && (age % 100 <= 15)) {
     alert('Вам ' + age + ' лет');
 } else if ((age % 10) === 1) {
     alert('Вам ' + age + ' год');
 } else if ((age % 10) < 5) {
     alert('Вам ' + age + ' года');
-} 
+}
